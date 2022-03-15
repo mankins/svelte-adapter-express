c74d2ca088a62378753bd090deb18829cb77e233
bowedyapper <bowedyapper@live.co.uk>
2022-03-15 02:02:16 +0000

    Deconstructed utils is now just builder, also renamed functions

--- index.js
+++ index.js
@@ -9,28 +9,32 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
  *   out?: string;
  * }} options
  */
-export default function ({ out = 'build', assets = 'assets', serverFile = `${__dirname}/files/server.js` } = {}) {
-	/** @type {import('@sveltejs/kit').Adapter} */
-	const adapter = {
-		name: '@mankins/svelte-adapter-express',
+export default function({
+  out = 'build',
+  assets = 'assets',
+  serverFile = `${__dirname}/files/server.js`
+} = {}) {
+  /** @type {import('@sveltejs/kit').Adapter} */
+  const adapter = {
+    name: '@mankins/svelte-adapter-express',
 
-		async adapt({utils}) {
-			utils.log.minor(`Copying assets to ${assets}`);
-			const static_directory = join(out, assets);
-			utils.copy_client_files(static_directory);
-			utils.copy_static_files(static_directory);
+    async adapt(builder) {
+      builder.log.minor(`Copying assets to ${assets}`);
+      const static_directory = join(out, assets);
+      builder.writeClient(static_directory);
+      builder.writeStatic(static_directory);
 
-			utils.log.minor('Copying server');
-			utils.copy_server_files(out);
+      builder.log.minor('Copying server');
+      builder.writeServer(out);
 
-			copyFileSync(serverFile, `${out}/index.js`);
+      copyFileSync(serverFile, `${out}/index.js`);
 
-			utils.log.minor('Prerendering static pages');
-			await utils.prerender({
-				dest: `${out}/prerendered`
-			});
-		}
-	};
+      builder.log.minor('Prerendering static pages');
+      await builder.writePrerendered({
+        dest: `${out}/prerendered`
+      });
+    }
+  };
 
-	return adapter;
+  return adapter;
 };
